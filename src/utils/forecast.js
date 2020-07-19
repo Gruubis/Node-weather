1a17d20ff9df6d7177f21fa9a1d8e7f78be4805f
unknown <s037814@ad.viko.lt>
2020-07-19 13:48:38 +0300

    final commit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,4 @@
-// const url = 'http://api.weatherstack.com/current?access_key=ce53a69cc92e6dec8cd0a1019f1d39aa&query=40.416775,-3.703790'
 
-// request({ url: url, json: true}, (error, response)=>{
-//     if(error){
-//         console.log(chalk.red.inverse('Unable to connect to weather services!'))
-//     }else if(response.body.error){
-//         console.log(chalk.red.inverse(response.body.error.info))
-//     }
-//     else{
-
-//         console.log(chalk.green.bold('In ' + response.body.current.weather_descriptions[0] +  " Current temperature is " + response.body.current.temperature + ' And it feels like ' + response.body.current.feelslike))
-//     }
 const request = require('request')
 
 const forecast = (longitude, latitude, callback)=> {
